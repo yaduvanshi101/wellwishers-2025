596ae3767cd0b56310979b60be79a763114f0d08
vice <vicemark108@gmail.com>
2025-05-21 19:16:54 +0530

    remove ThemeProvider component from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from "react";
 import "@/app/globals.css";
 import {Inter} from "next/font/google";
-import {ThemeProvider} from "@/components/theme-provider";
 import {Navigation} from "@/components/navigation";
 import {Footer} from "@/components/footer";
 
@@ -18,16 +17,9 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     return (
         <html lang="en" suppressHydrationWarning>
             <body className={inter.className}>
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="dark"
-                    enableSystem
-                    disableTransitionOnChange
-                >
-                    <Navigation />
-                    {children}
-                    <Footer />
-                </ThemeProvider>
+                <Navigation />
+                {children}
+                <Footer />
             </body>
         </html>
     );
